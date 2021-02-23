dbfbca31bd9cde58aa2a57f9b86bb28fff2cc2d8
Nathan Cuvellier <cuvellier.nathan@gmail.com>
2021-02-23 22:02:16 +0100

    add layout connection

--- app/src/main/java/fr/test200/findme/MainActivity.kt
+++ app/src/main/java/fr/test200/findme/MainActivity.kt
@@ -2,10 +2,18 @@ package fr.test200.findme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val test : Button = findViewById(R.id.btn_connection)
+
+        test.setOnClickListener {
+            Toast.makeText(this, test.text, Toast.LENGTH_LONG).show()
+        }
     }
 }
