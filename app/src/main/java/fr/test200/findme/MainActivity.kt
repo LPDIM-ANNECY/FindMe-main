c2dd5a8390f1387b61f5729d43db6946664ee998
Loic Viard Crétat <loicviard12@gmail.com>
2021-02-23 18:18:19 +0100

    Add dependencies, plugins, LoginFragment and navigation

--- app/src/main/java/fr/test200/findme/MainActivity.kt
+++ app/src/main/java/fr/test200/findme/MainActivity.kt
@@ -2,10 +2,16 @@ package fr.test200.findme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_activity)
+        navController = findNavController(R.id.nav_host_fragment)
     }
 }
---
+++ app/src/main/java/fr/test200/findme/login/LoginViewModelFactory.kt
@@ -0,0 +1,14 @@
+package fr.test200.findme.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class LoginViewModelFactory() : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel() as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
